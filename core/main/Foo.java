0620f90d1df779bed3ff20cc8a0fb98525bfe45f
johncurcio <joao8177@gmail.com>
2017-06-16 21:11:03 -0300

    Add examples dependencies

--- core/main/Foo.java
+++ core/main/Foo.java
@@ -5,18 +5,18 @@ import java.util.HashSet;
 public class Main {
 	Integer a = null;
 	private int a = 10;
-	boolean b = 100 == 10;
+	boolean b = 100 + 10;
 	boolean x, y = false;
 
 	protected int a(){
-		double j = 1 + 1;
+		double j = 1 - 1;
 		if (j > 1){
 			return 2;
 		}		
 		return 1;
 	}
 	
-	public static void main(String[] args){
+	public static void main(String args){
 		
 	}
 }
